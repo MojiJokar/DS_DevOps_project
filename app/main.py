29566214410e8534eba38cd79244b407be7ae2e9
ds_devops_project <mmb2020max@gmail.com>
2025-06-25 15:10:38 +0200

    all files added

--- app/main.py
+++ app/main.py
@@ -1,26 +1,6 @@
-print(">>> main.py loaded <<<")          # <--- Add this as the first line
-
+from typing import Union
 from fastapi import FastAPI
-from app.db import database, User
-
-app = FastAPI(title="FastAPI, Docker, and Traefik")
-
-@app.get("/ping")
-async def ping():
-    return {"message": "pong"}
-
+app = FastAPI()
 @app.get("/")
-async def read_root():
-    return await User.objects.all()
-
-@app.on_event("startup")
-async def startup():
-    if not database.is_connected:
-        await database.connect()
-    # create a dummy entry
-    await User.objects.get_or_create(email="test@test.com")
-
-@app.on_event("shutdown")
-async def shutdown():
-    if database.is_connected:
-        await database.disconnect()
+def read_root():
+    return {"Hello": "We Love Datascientest !!"}
